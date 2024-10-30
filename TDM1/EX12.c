a89a7eafc912dd7d6c348f78c62891fc6b34a9f7
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-10-30 09:10:32 +0100

    ajouter ex12 et 13

---
+++ TDM1/EX12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char B[3];
+    int motpass;
+    while (B != "bon")
+    {
+        motpass = (rand() % (9999+1-1000)) + 1000; // il faut trouve 4 entiers (min = 1000 et max = 9999)
+        printf("Le mot de passe généré est : %d\n", motpass);
+        printf("Si vous aimez le mot de passe, tapez bon : ");
+        scanf("%c", &B);
+        break;
+    }
+    return 0;
+}
