8b9a5685cd433929bf0688e7a02ec3cd4e3c0a63
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-11-07 10:23:50 +0100

    ajouter exercice 5

---
+++ TDM2/EX5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int N, A;
+    printf("\nEntrer Le Nombre des elements dans cette tableau : ");
+    scanf("%d", &N);
+    int T[N];
+    int C[N];
+
+    for (int i = 0; i < N ; i++)
+    {
+        printf("\nEntrer L'element %d : ", i+1);
+        scanf("%d", &T[i]);        
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int X = 1;
+        for (int j = i + 1; j < N; j++)
+        {
+            if (T[i] == T[j])
+            {
+                X++;
+                C[j] = -1; // Marquer les doublons
+            }
+        }
+        if (X > 1 && C[i] != -1) // Si l'element est un doublon non marque
+        {
+            printf("\nL'element %d est apparait %d fois.\n", T[i], X);
+        }
+    }
+    return 0;
+}
