44f3f8f0e4fd14ebf8981dba33506ccaefda7803
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-11-11 07:58:41 +0100

    ajouter ex17

---
+++ TDM2/EXS17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char A[100];
+    char B[100];
+    printf("Entrer Un chaine A : ");
+    gets(A);
+
+    strncpy(B, A, 3);
+
+    printf("L'autre chaine à copie les premiers 3 caractere est : ");
+    puts(B);
+
+    return 0;
+}
