54491c7051b24e93bb5ce0f39935e0b2b2a05322
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-12-14 21:07:14 +0100

    corriger

--- TDM4/EXS13.c
+++ TDM4/EXS13.c
@@ -1,18 +1,18 @@
 // Nom : Mohamed Talhaoui
 // Filière : IIA (S3) | 2024/2025
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
     char *P[] = {"Salam", "Si", "Mohamed", "Talhaoui"};
 
+    int taille = sizeof(P) / sizeof(P[0]);
+
     printf("les elements de tableau sont : \n");
     
-    for (int i = 0; i < strlen(*P) - 1; i++)
+    for (int i = 0; i < taille - 1; i++)
     {
-        printf("%s", P[i]);
-        printf(" ");
+        printf("%s ", P[i]);
     }
     return 0;
 }
