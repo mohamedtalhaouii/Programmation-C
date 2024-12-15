f2247c81630cb2cb0af73d1bd0fbad7750bf383d
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-12-15 09:32:27 +0100

    ajouter les commentaires et corriger des erreurs

--- TDM4/EXS14.c
+++ TDM4/EXS14.c
@@ -10,7 +10,8 @@ int main()
 
     int taille = sizeof(P) / sizeof(P[0]);
 
-//-----------------Trie a Bulles--------------------//
+//-------------------Trie a Bulles--------------------//
+//--Vous pouvez utiliser n'importe quel type de Tri--//
     for (int i = 0; i < taille - 1; i++)
     {
         for (int j = 0; j < taille - i - 1; j++)
--- TDM4/EXS19.c
+++ TDM4/EXS19.c
@@ -1,3 +1,5 @@
+// Nom : Mohamed Talhaoui
+// Filière : IIA (S3) | 2024/2025
 #include <stdio.h>
 
 void swap(int *A, int *B){
@@ -10,16 +12,19 @@ int main()
 {
     int A, B;
 
+    // Demande de saisir :
     printf("Entrer la valeur de A : ");
     scanf("%d", &A);
     printf("Entrer la valeur de B : ");
     scanf("%d", &B);
 
+    // Affiche avant l'echange :
     printf("Avant l'echange : \n");
     printf("A = %d et B = %d", A, B);
 
     swap(&A, &B);
 
+    // Affiche apres l'echange :
     printf("\nApres l'echange : \n");
     printf("A = %d et B = %d", A, B);
     return 0;
